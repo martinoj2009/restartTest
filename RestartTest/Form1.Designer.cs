f72ec812a66b3f97506de3bca4b1dad2c42e2c84
Martino <martinojones_2009@hotmail.com>
2016-03-18 12:03:01 -0400

    Initial commit

---
+++ RestartTest/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace RestartTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BW1 = new System.ComponentModel.BackgroundWorker();
+            this.restartLabel = new System.Windows.Forms.Label();
+            this.restartTime = new System.Windows.Forms.Label();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.removeStartupButton = new System.Windows.Forms.Button();
+            this.addStartupButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.logCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // BW1
+            // 
+            this.BW1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BW1_DoWork);
+            this.BW1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BW1_RunWorkerCompleted);
+            // 
+            // restartLabel
+            // 
+            this.restartLabel.AutoSize = true;
+            this.restartLabel.Location = new System.Drawing.Point(39, 44);
+            this.restartLabel.Name = "restartLabel";
+            this.restartLabel.Size = new System.Drawing.Size(72, 13);
+            this.restartLabel.TabIndex = 0;
+            this.restartLabel.Text = "Restarting in: ";
+            // 
+            // restartTime
+            // 
+            this.restartTime.AutoSize = true;
+            this.restartTime.Location = new System.Drawing.Point(117, 44);
+            this.restartTime.Name = "restartTime";
+            this.restartTime.Size = new System.Drawing.Size(13, 13);
+            this.restartTime.TabIndex = 1;
+            this.restartTime.Text = "0";
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(11, 80);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(84, 70);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "STOP";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // removeStartupButton
+            // 
+            this.removeStartupButton.Location = new System.Drawing.Point(13, 156);
+            this.removeStartupButton.Name = "removeStartupButton";
+            this.removeStartupButton.Size = new System.Drawing.Size(82, 70);
+            this.removeStartupButton.TabIndex = 3;
+            this.removeStartupButton.Text = "Remove Startup";
+            this.removeStartupButton.UseVisualStyleBackColor = true;
+            this.removeStartupButton.Click += new System.EventHandler(this.removeStartupButton_Click);
+            // 
+            // addStartupButton
+            // 
+            this.addStartupButton.Location = new System.Drawing.Point(101, 156);
+            this.addStartupButton.Name = "addStartupButton";
+            this.addStartupButton.Size = new System.Drawing.Size(82, 70);
+            this.addStartupButton.TabIndex = 4;
+            this.addStartupButton.Text = "Add Startup";
+            this.addStartupButton.UseVisualStyleBackColor = true;
+            this.addStartupButton.Click += new System.EventHandler(this.addStartupButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(99, 80);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(84, 70);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "RESTART";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // logCheckBox
+            // 
+            this.logCheckBox.AutoSize = true;
+            this.logCheckBox.Location = new System.Drawing.Point(86, 232);
+            this.logCheckBox.Name = "logCheckBox";
+            this.logCheckBox.Size = new System.Drawing.Size(44, 17);
+            this.logCheckBox.TabIndex = 6;
+            this.logCheckBox.Text = "Log";
+            this.logCheckBox.UseVisualStyleBackColor = true;
+            this.logCheckBox.CheckStateChanged += new System.EventHandler(this.logCheckBox_CheckStateChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 261);
+            this.Controls.Add(this.logCheckBox);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.addStartupButton);
+            this.Controls.Add(this.removeStartupButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.restartTime);
+            this.Controls.Add(this.restartLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Restart Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker BW1;
+        private System.Windows.Forms.Label restartLabel;
+        private System.Windows.Forms.Label restartTime;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button removeStartupButton;
+        private System.Windows.Forms.Button addStartupButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.CheckBox logCheckBox;
+    }
+}
+
---
+++ RestartTest/Form1.cs
@@ -0,0 +1,256 @@
+﻿using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace RestartTest
+{
+    public partial class Form1 : Form
+    {
+        public static bool stop = false;
+        public static string programPath = System.IO.Path.GetFullPath(Application.ExecutablePath);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Check to see if a log file exists
+            if(File.Exists(Application.StartupPath + "\\log.txt"))
+            {
+                //The log file exists, log the start and restart
+                logCheckBox.Checked = true;
+            }
+
+            main();
+        }
+
+
+        private void main()
+        {
+            writeLog("Application started");
+            restartTime.Text = "15";
+            BW1.WorkerSupportsCancellation = true;
+            BW1.RunWorkerAsync();
+
+            startButton.Enabled = false;
+            stopButton.Enabled = true;
+
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BW1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //What ever the timer is set that to the counter
+            int counter = Int32.Parse(restartTime.Text);
+
+            while (counter > 0)
+            {
+                //Break if stop has been set
+                if(stop == true)
+                {
+                    return;
+                }
+
+                Invoke(new Action(() =>
+                restartTime.Text = counter.ToString()));
+
+                Thread.Sleep(1000);
+
+                counter--;
+            }
+
+            //Invoke again to show 0
+            Invoke(new Action(() =>
+                restartTime.Text = counter.ToString()));
+
+            if(restartSystem())
+            {
+                writeLog("Restarting");
+                Invoke(new Action(() =>
+                restartLabel.Text = "Restarting!"));
+
+                Invoke(new Action(() =>
+                restartTime.Visible = false));
+            }
+
+            return;
+
+        }
+
+        private bool restartSystem()
+        {
+            //If stop is set then don't cont restart
+            if(stop == true)
+            {
+                return false;
+            }
+
+            //Launch the restart with no window 
+            System.Diagnostics.Process launch = new System.Diagnostics.Process();
+            launch.StartInfo.FileName = @"C:\Windows\System32\shutdown.exe";
+            launch.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            launch.StartInfo.CreateNoWindow = true;
+            launch.StartInfo.Arguments = "/r /t 0";
+
+            try
+            {
+                launch.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            //Press this button to stop the countdown and quit
+            stop = true;
+            BW1.CancelAsync();
+
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+        }
+
+        private void removeStartupButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Only perform this task if the startup key esists
+                if (startupExists() == true)
+                {
+                    RegistryKey startupItems = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+                    startupItems.DeleteValue("RestartTest");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void addStartupButton_Click(object sender, EventArgs e)
+        {
+            //As long as there's not an entry in the startup then add one
+            if(startupExists() == false)
+            {
+                try
+                {
+                    //Entry doesn't exist
+                    RegistryKey startupItems = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+                    startupItems.CreateSubKey("RestartTest");
+                    startupItems.SetValue("RestartTest", programPath, RegistryValueKind.String);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+            }
+        }
+
+        private bool startupExists()
+        {
+            try
+            {
+                RegistryKey startupItems = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+
+                foreach (string item in startupItems.GetValueNames())
+                {
+                    if (item == "RestartTest")
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+
+            return false;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void BW1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //Reset the timer
+            Invoke(new Action(() =>
+                restartTime.Text = "15"));
+        }
+
+        private void writeLog(string message)
+        {
+            //Only do this if loggin is checked
+            if(logCheckBox.Checked == true)
+            {
+                //This function will write the log for the restart if the user would like to keep track
+                try
+                {
+                    System.IO.File.AppendAllText(Application.StartupPath + "\\log.txt", DateTime.Now.ToString() + " " + message + Environment.NewLine);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+            }
+            
+        }
+
+        private void logCheckBox_CheckStateChanged(object sender, EventArgs e)
+        {
+            if(logCheckBox.Checked == true)
+            {
+                if(File.Exists(Application.StartupPath + "\\log.txt") == false)
+                {
+                    try
+                    {
+                        //Create the file since it doesn't exist
+                        FileStream file = File.Create(Application.StartupPath + "\\log.txt");
+                        file.Close();
+                        writeLog("Logging enabled");
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    
+                }
+            }
+            else
+            {
+                if(File.Exists(Application.StartupPath + "\\log.txt"))
+                {
+                    //The check box has been unchecked, so delete the log
+                    try
+                    {
+                        File.Delete(Application.StartupPath + "\\log.txt");
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
